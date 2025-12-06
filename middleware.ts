0ac0a62a041fa203182b35f538ac4538a2c6fadf
Pepoo Aziz <mylogisticeg@gmail.com>
2025-12-06 16:41:31 +0200

    Refactor middleware to use next-intl package

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,11 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+// middleware.ts
+import createMiddleware from "next-intl/middleware";
 
-const locales = ['ar', 'en', 'zh'];
-const defaultLocale = 'ar';
-
-export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-
-    // Check if pathname is missing locale
-    const pathnameIsMissingLocale = locales.every(
-        (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-    );
-
-    // Redirect if there is no locale
-    if (pathnameIsMissingLocale) {
-        // Redirect to default locale
-        return NextResponse.redirect(
-            new URL(`/${defaultLocale}${pathname}`, request.url)
-        );
-    }
-}
+export default createMiddleware({
+  locales: ["en", "ar", "zh"],
+  defaultLocale: "en",
+});
 
 export const config = {
-    matcher: [
-        // Skip all internal paths (_next, api, etc)
-        '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*|_next).*)',
-    ],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
