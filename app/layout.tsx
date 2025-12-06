e50b86531bf9cf131cc05f58f4347b3b03937835
Pepoo Aziz <mylogisticeg@gmail.com>
2025-12-06 23:33:44 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,9 @@
-export const dynamic = "force-dynamic";
+import "./globals.css";
 
-import { NextIntlClientProvider } from "next-intl";
-import { notFound } from "next/navigation";
-
-export default async function LocaleLayout({ children, params }) {
-  const { lang } = params;
-
-  const locales = ["en", "ar", "zh"];
-
-  if (!locales.includes(lang)) notFound();
-
-  const messages = (await import(`../../messages/${lang}.json`)).default;
-
+export default function RootLayout({ children }) {
   return (
-    <html lang={lang} dir={lang === "ar" ? "rtl" : "ltr"}>
-      <body>
-        <NextIntlClientProvider locale={lang} messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
